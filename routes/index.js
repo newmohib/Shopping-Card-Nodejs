0bbfa567f6ac53dd140e2922309d441965dd4b92
newmohib <newmohib@gmail.com>
2020-01-05 00:42:07 +0600

    get data for shopping cart to database

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var Product=require('../models/product')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('shop/index', { title: 'Express' });
+  Product.find().then(result=>{
+    res.render('shop/index', { title: 'Shopping Cart',products:result });
+
+  }).catch(err=>{
+    console.log("err",err);
+  })
 });
 
 module.exports = router;
