46d9f3fec366266a1abc32f8ef57ffb681c80a45
newmohib <newmohib@gmail.com>
2020-01-11 15:05:55 +0600

    added signup and signin

--- config/passport.js
+++ config/passport.js
@@ -1,61 +1,36 @@
 var passport = require('passport')
 var LocalStrategy = require('passport-local').Strategy;
-var bcrypt = require('bcrypt');
+const bcrypt = require('bcrypt');
 var User = require('../models/user')
-
-
-let getUserById=(id)=>{
-  User.findOne({ _id: id }, (err, user) => {
-    console.log("getUserById",err,user);
-     if (err) {
-       return false;
-     }else{
-       return true;
-     }
-  })
-}
 
 function initialize() {
 
   const authenticateUser = (email, password, done) => {
-console.log("email password",email, password);
-    User.findOne({ email: email }, (err, user) => {
-
-      if (err) {
-        return done(null, false, { message: 'No user with that email' })
-      }
-
-      bcrypt.compare(password, user.password, function (err, result) {
-        console.log("Password Test",user, err, result);
+    User.findOne({ email: email })
+    .then((user)=>{
+      console.log("success",user);
+      bcrypt.compare(password, user.password, (err, result)=> {
+        console.log("Password Test", user, err, result);
         if (result) {
           return done(null, user)
         } else {
           return done(null, false, { message: 'Password incorrect' })
-
         }
       });
-
-      // try {
-      //     if (await bcrypt.compare(password,user.password)) {
-      //         return done(null, user)
-
-      //     } else {
-      //         return done(null, false, {message:'Password incorrect'})
-
-      //     }
-      // } catch (e) {
-      //     return done(e)
-
-      // }
-
+    }).catch(err=>{
+      return done(null, false, { message: 'No user with that email' })
     })
 
   }
   passport.use(new LocalStrategy({ usernameField: 'email' }, authenticateUser))
-  passport.serializeUser((user, done) => done(null, user._id))
-  passport.deserializeUser((id, done) =>  done(null, getUserById(id)))
+  passport.serializeUser( (user, done)=> { done(null, user.id); });
+
+  passport.deserializeUser( (id, done)=> {
+    User.findById(id, (err, user)=> {
+      done(err, user);
+    });
+  });
+
 }
-
-
 
 module.exports = initialize
